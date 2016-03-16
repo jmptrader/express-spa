5e424c8594f02b606971b2221a003fd0b49fe2f1
Erik Akers <erikakers@gmail.com>
2016-03-15 22:13:39 -0500

    basic express server set up

---
+++ development.js
@@ -0,0 +1,43 @@
+/* eslint new-cap: [2, {"capIsNewExceptions": ["Server"]}] */
+
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import webpack from 'webpack';
+
+const app = express();
+const http = require('http').Server(app);
+const port = process.env.PORT || 5000;
+const config = require('./webpack.config.dev');
+const compiler = webpack(config);
+
+app.use(morgan('dev'));
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    publicPath: config.output.publicPath,
+    stats: {
+        colors: true,
+        hash: false,
+        timings: true,
+        chunks: false,
+        chunkModules: false,
+        modules: false
+    }
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+http.listen(port, (error) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    console.log(`${process.env.NODE_ENV} server running on port: ${port}`);
+})
