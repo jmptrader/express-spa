1ccf17611e68628efd11a2a363c9e34142feb795
Erik Akers <erikakers@gmail.com>
2016-03-18 11:58:54 -0500

    changing directory names to limit confusion

--- client/index.js
+++
@@ -1,4 +0,0 @@
-document.write('Hello Webpack testing');
-
-console.log('hot reloading');
-console.log('testing 1');
---
+++ src/index.js
@@ -0,0 +1,11 @@
+// DO NOT REMOVE
+// Needed for Hot Reloading
+// TODO: Should only working when ENV is development
+if (module.hot) {
+  module.hot.accept();
+}
+
+document.write('Hello Webpack');
+
+console.log('hot reloading test');
+console.log('testing hot');
