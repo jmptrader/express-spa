5eb219fb1d5ee2add9d6d4b9a3e96f49be326827
Erik Akers <erikakers@gmail.com>
2016-03-18 15:58:23 -0500

    options split into functions

--- webpack.factory.js
+++ webpack.factory.js
@@ -4,34 +4,94 @@ var path = require('path');
 module.exports = function(options) {
 
     return {
-        devtool: 'cheap-module-eval-source-map',
-        entry: [
-            'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true',
-            './src/index'
-        ],
-        resolve: {
-            alias: {
-                components: path.resolve(__dirname, 'client/components')
-            },
-            extensions: ['', '.js', '.ts']
-        },
-        output: {
-            path: path.join(__dirname, 'public'),
-            filename: 'app.js',
-            publicPath: '/assets/'
-        },
-        plugins: [
-            new webpack.optimize.CommonsChunkPlugin('commons', 'commons.js'),
-            new webpack.optimize.OccurenceOrderPlugin(),
-            new webpack.HotModuleReplacementPlugin(),
-            new webpack.NoErrorsPlugin()
-        ],
+        entry: entry(options),
+        resolve: resolve(options),
+        output: output(options),
+        plugins: plugins(options),
         module: {
-            loaders: [{
-                test: /\.js$/,
-                loaders: ['babel'],
-                exclude: /node_modules/,
-            }]
+            loaders: loaders(options)
         }
     }
 };
+
+/*
+ Entry Files for the Webpack configuation
+ Additional Entry Files can be defined in the Files Array
+ */
+function entry(options) {
+    var files = [
+        './src/index'
+    ];
+
+    if (options.env === 'development') {
+        files.unshift('webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=true');
+    }
+
+    return files;
+}
+
+/*
+    Returns Webpack Resolve Object configuation
+ */
+function resolve(options) {
+    return {
+        extensions: ['', '.js', '.ts']
+    }
+}
+
+/*
+    Returns Webpack's Output Object configuation
+ */
+function output(options) {
+    return {
+        path: path.join(__dirname, 'public'),
+        filename: 'app.js',
+        publicPath: '/assets/'
+    }
+}
+
+/*
+    Return Plugins based on Environment
+    TODO: implement SASS compiling
+    https://github.com/webpack/extract-text-webpack-plugin/blob/master/example/webpack.config.js
+ */
+function plugins(options) {
+    var plugins = [];
+
+    plugins.push(new webpack.optimize.CommonsChunkPlugin('commons', 'commons.js'));
+    plugins.push(new webpack.optimize.OccurenceOrderPlugin());
+    plugins.push(new webpack.NoErrorsPlugin());
+
+    if (options.env === 'development') {
+        plugins.push(new webpack.HotModuleReplacementPlugin());
+    }
+
+    if (options.env === 'production') {
+        plugins.push(new webpack.optimize.DedupePlugin());
+    }
+
+    return plugins;
+}
+
+/*
+    Returns Loaders Objects
+ */
+function loaders(options) {
+    var loaders = [];
+
+    if (!options.typescript) {
+        loaders.push({
+            test: /\.js(x)?$/,
+            loaders: ['babel'],
+            exclude: /node_modules/,
+        })
+    } else {
+        loaders.push({
+            test: /\.ts(x)?$/,
+            loaders: ['babel!ts'],
+            exclude: /node_modules/
+        });
+    }
+
+    return loaders;
+}
