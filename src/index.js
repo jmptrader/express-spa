9c21c6795a1675f9574b0e0fb1ab062cdf2ae874
Erik Akers <erikakers@gmail.com>
2016-03-18 12:22:51 -0500

    moving hot reloading function to the bottom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+document.write('Hello Webpack');
+
+console.log('hot reloading test');
+console.log('testing hot');
+
+
+
 // DO NOT REMOVE
 // Needed for Hot Reloading
 // TODO: Should only working when ENV is development
 if (module.hot) {
   module.hot.accept();
 }
-
-document.write('Hello Webpack');
-
-console.log('hot reloading test');
-console.log('testing hot');
