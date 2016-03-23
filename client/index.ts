8648b35ded4db7341a150fb7f6e1b836cc386b3a
Erik Akers <erikakers@gmail.com>
2016-03-23 15:07:17 -0500

    adding dev variable for hmr

--- client/index.ts
+++ client/index.ts
@@ -2,17 +2,17 @@ function greeter(person: string) {
     return "Hello, " + person;
 }
 
-var user = "Jane User";
+var user = "Express Spa";
 
 document.body.innerHTML = greeter(user);
 
 console.log('hot reloading test');
-console.log('testing hot');
+console.log('testing hot with Typescript');
 
 // DO NOT REMOVE
 // Needed for Hot Reloading
-// TODO: Should only working when ENV is development
-// erroring out in Typescript
-if (module.hot) {
-  module.hot.accept();
+if (DEV) {
+    if (module.hot) {
+        module.hot.accept();
+    }
 }
